bd80bdb18961e9632291e0f193e19866e3e05ee8
philipp sauerlaender <philipp.sauerlaender@dci-student.org>
2024-07-11 15:24:53 +0200

    google meets clone

--- src/components/VideoGrid.js
+++ src/components/VideoGrid.js
@@ -5,14 +5,14 @@ import VideoTile from "./VideoTile";
 function VideoGrid() {
     // Beispiel-Teilnehmer
     const participants = [
-        { id: 1, name: "Stefan Kirsch", initial: "S" },
+        { id: 1, name: "Julijana Uneva Kjumbeva", initial: "J" },
         { id: 2, name: "Ralf Schmitz", initial: "R" },
         { id: 3, name: "Marius Weber", initial: "M" },
         { id: 4, name: "Paul Schilling", initial: "P" },
         { id: 5, name: "Philipp Sauerlaender", initial: "P" },
-        { id: 6, name: "Johannes Müller", initial: "J" },
-        { id: 7, name: "Lena Schmidt", initial: "L" },
-        { id: 8, name: "Katharina Schuster", initial: "K" },
+        { id: 6, name: "Kevin Lampe", initial: "L" },
+        { id: 7, name: "Manuel Fahrenholz", initial: "M" },
+        { id: 8, name: "Oscar van Velsen", initial: "O" },
     ];
 
     return (
